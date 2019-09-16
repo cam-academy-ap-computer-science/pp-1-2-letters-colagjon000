de1390f983a3fd4c1e1c90dd60e131bfd0abc130
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-16 07:29:30 -0700

    Initial Start. Still brainstorming.

--- src/Letters.java
+++ src/Letters.java
@@ -5,5 +5,17 @@ public class Letters {
 		// TODO Auto-generated method stub
 
 	}
+	public static void greetingPara() {
+		System.out.println("Hello!\nI hope that the beginning of Autumn is treating you well.");
+	}
+	public static void endingpara() {
+		System.out.println("I am waiting earnestly for your reply,\n\nSincerely,\nJonah");
+	}
+	public static void friendLetter() {
+		System.out.println("Dear Kevin,\n"); //friend
+		greetingPara();
+		System.out.println("How has the school year been going for you?\nThe beginning of the school year has been going well for me");
+		System.out.println("");
+	}
 
 }
